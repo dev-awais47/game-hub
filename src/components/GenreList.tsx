b80becd235d7ddf3d485bacddaaa44ef9e5807c4
Muhammad Awais <muhammadawaisijaz010@gmail.com>
2024-05-20 14:14:02 +0500

    Display Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { genres } = useGenres();
   return (
     <>
-      <ul>
-        {genres.map(genre => <li key={genre.id}>{genre.name}</li> )}
-      </ul>
+      <List>
+        {genres.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Text>{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 };
