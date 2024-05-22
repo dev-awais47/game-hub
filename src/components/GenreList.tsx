b15af072d06b48e4e841cf5889dc2e3f46001fb1
Muhammad Awais <muhammadawaisijaz010@gmail.com>
2024-05-22 14:33:47 +0500

    make changes in the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -20,16 +21,18 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   if (isLoading) return <Spinner />;
   return (
     <>
+      <Heading fontSize='2xl' marginBottom={3}>Genre</Heading>
       <List>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
                 boxSize="32px"
+                objectFit='cover'
                 borderRadius={8}
                 src={getCroppedImageUrl(genre.image_background)}
               />
-              <Button color='dark' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal' } onClick={() => onSelectGenre(genre)} variant="link">
+              <Button whiteSpace='normal' textAlign='left' color='dark' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal' } onClick={() => onSelectGenre(genre)} variant="link">
                 {genre.name}
               </Button>
             </HStack>
